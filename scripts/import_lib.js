14527be183363baba5188a161c5df4caeda22ccf
JovialGang <333412@niuitmo.ru>
2023-12-04 22:35:07 +0300

    done lab7.2

--- scripts/import_lib.js
+++ scripts/import_lib.js
@@ -1,16 +1,27 @@
 function showModal() {
     Swal.fire({
-        title: 'Окно открыто',
-        text: 'Теперь его можно закрыть.',
+        title: 'Ура',
+        text: 'Теперь у вас есть друзья.',
         icon: 'success',
         confirmButtonText: 'OK'
     });
 }
 
 // Инициализация Swiper слайдера
+// var swiper = new Swiper('.swiper-container', {
+//     slidesPerView: 1,
+//     spaceBetween: 10,
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev'
+//     }
+// });
+
+// Инициализация Swiper слайдера
 var swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
     spaceBetween: 10,
+    centeredSlides: true,  // Добавляем эту опцию
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
